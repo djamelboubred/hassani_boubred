359e8f1e225929c8f3c95688e862e7705310192d
djamelboubred <djamelboubred.saclay@gmail.com>
2022-01-06 17:03:02 +0100

    nouvelle version 2.0

--- recherche_sequence_codante.c
+++ recherche_sequence_codante.c
@@ -1,6 +1,73 @@
 #include "utils.h"
 #define TRUE 0
 #define FALSE 1
+int recherche_max_cds(char sequence[], int* position, int cadre){
+	int j=0; // compteur du nombre de nt
+	int k = 0; // position initial du premier nt de la cds
+	int maximum =0;   // le nombre le plus important de nt dans une cds
+	int taille = strlen(sequence);
+	int fin = FALSE;
+	int i = cadre;	
+	int seq_complet = TRUE;
+//	printf("test valeur de i:%d\n",i);
+//	printf("sequence : %s\n",sequence);
+//	printf("taille : %d\n",taille);
+	while((j==0)||(i<taille-2)){ 	
+        	//printf("%d",i); fonctionne
+		//printf("test dans la boucle"); fonctionne ici
+		if ((sequence[i]=='A') && (sequence[i+1]=='T') && (sequence[i+2]=='G')){ // si on trouve notre codon start
+                	// printf("test dans le codon start");// fonctionne 
+			j=3;
+                        k=i;    // on prend la position du codon start
+                        while((i<taille-2)||(fin!=TRUE)){
+			//	printf("entrée seconde boucle");fonctionne
+                        	if((sequence[i]=='T') && (sequence[i+1]=='A') && (sequence[i+2]=='A')){
+                                	fin=TRUE;
+                                        break;
+                                }
+                                if((sequence[i]=='T') && (sequence[i+1]=='A') && (sequence[i+2]=='G')){
+                                	fin=TRUE;
+                                        break;
+                                }
+                                if((sequence[i]=='T') && (sequence[i+1]=='G') && (sequence[i+2]=='A')){
+                                	fin=TRUE;
+                                        break;
+                                }
+                                if((sequence[i]=='\0')){
+                                	fin=TRUE;
+                                        seq_complet = FALSE;
+                                        break;
+                                }
+                                if((sequence[i+1]=='\0')){
+                                	fin=TRUE;
+                                        seq_complet = FALSE; //FALSE car la séquence n'est pas complete donc on ne la prend pas en compte
+                                        break;
+                                }
+				if((sequence[i+2]=='\0')){
+	 				fin=TRUE;
+                                        seq_complet = FALSE;
+                                        break;
+                                }  
+				i= i+3;
+   				j= j+3; 
+			}
+			//printf("test de mi parcours après econde boucle");fonctionne
+                        if(seq_complet==FALSE){
+                        	j=0;
+                                seq_complet = TRUE;
+                        }
+	//	printf("test fin de boucle\n"); fonctionne
+                if(maximum<j){ // si on a une cds de taille plus importante
+                 	maximum = j;
+                        printf("test max %d\n",maximum);// test de la plus grande
+                        position =&k;
+			printf("positiob dans fct = %d\n",*position);
+                }
+		}
+	i=i+3;
+	}
+	return maximum;
+}
 
 void sequence_complementaire(char sequence[], int taille, char sequence_comp[]){ //prend ue séquence et la taille de la séquence en argument
 
@@ -31,9 +98,9 @@ void sequence_complementaire(char sequence[], int taille, char sequence_comp[]){
 
 void recherche_sequence_codante(char sequence[], int taille){
 
-	char* seq  = malloc(sizeof(char)*taille);
+	char* seq  = malloc(sizeof(char)*taille);// => mettre cds à la place 
 	char* sequence_comp = malloc(sizeof(char)*taille);
-	int debut = TRUE; //si on commence 0 sinon 1 
+//	int debut = TRUE; //si on commence 0 sinon 1 
 	int k =0; // position du premier nucléotide afin de pouvoir initier l'écriture de notre cds la plus grande lors de la fin du traitement de notre séquence  
 	int j = 0; //compteur nombres de nt
 	int i =0; // compteur en respectant le cadre de lecture et la lecture de codon(+3) à chaque tour
@@ -41,12 +108,83 @@ void recherche_sequence_codante(char sequence[], int taille){
 	int brin_sens = TRUE; //Valeur 0 si on est dans le cas du brin sens et 1 si brin anti-sens
 	int cadre_lecture= 0; // Indique à l'utilisateur dans qu'elle cadre de lecture se trouve la  plus grande CDS
 	int max_brin=TRUE;	//permet de connaître le brin pris en compte pour notre cds la plus grane 
-	int position = k;
-	int fin = FALSE;
-	int seq_complet = TRUE;
+	int position = 0; //position définitif de la plus grande cds
+//	int fin = FALSE;
+	int cadre_max;
+//	int seq_complet = TRUE; // gestion erreur si \n présent dans la sequence
 
 	sequence_complementaire(sequence,taille,sequence_comp);
-		while(fin == FALSE){//On procéde dans l'odre le cadre de lecture 0,+1,+2 puis on change de brin et on remodifie le cadre de lecture
+	// test
+//	printf("complementaire: %s\n",sequence_comp);
+/*	char path_output[20] = "complementaire.fasta";
+	save_sequence(path_output,sequence_comp);*/
+	if(brin_sens == TRUE){
+	//	printf("test entrée fonctin recherche cds\n");
+		j = recherche_max_cds(sequence,&k,cadre_lecture);
+	//	printf("test1");
+		if(max<j){
+			max=j;
+			printf("valeur de k:%d\n",k);
+			position = k;
+			cadre_max =cadre_lecture;	
+			max_brin = brin_sens;
+		}
+		printf("position = %d\n",position);
+		cadre_lecture++;
+		j = recherche_max_cds(sequence,&k,cadre_lecture);
+	//	printf("test2");
+		if(max<j){
+			max=j;
+			position= k;
+			cadre_max = cadre_lecture;
+			max_brin = brin_sens;
+
+		}
+		printf("position = %d\n", position);
+		cadre_lecture++;
+		j = recherche_max_cds(sequence,&k,cadre_lecture);
+	//	printf("test3");
+		if(max<j){
+			max=j;
+			position=k;
+			cadre_max = cadre_lecture;
+			max_brin = brin_sens;
+		}
+		brin_sens = FALSE;
+	}
+	if(brin_sens == FALSE){
+		cadre_lecture = 0; //on réinitialise le cadre de lecture
+		j = recherche_max_cds(sequence_comp,&k,cadre_lecture);
+	//	printf("test4");
+                if(max<j){
+                        max=j;
+                        position=k;
+                        cadre_max =cadre_lecture;
+                        max_brin = brin_sens;
+                }
+		printf("position = %d\n",position);
+                cadre_lecture++;
+                j = recherche_max_cds(sequence_comp,&k,cadre_lecture);
+	//	printf("test5");
+                if(max<j){
+                        max=j;
+                        position=k;
+                        cadre_max = cadre_lecture;
+                        max_brin = brin_sens;
+                }
+		printf("position = %d\n",position);
+                cadre_lecture++;
+                j = recherche_max_cds(sequence_comp,&k,cadre_lecture);
+	//	printf("test6");
+                if(max<j){
+                        max=j;
+                        position=k;
+                        cadre_max = cadre_lecture;
+                        max_brin = brin_sens;
+                }
+	}
+
+	/*	while(fin == FALSE){//On procéde dans l'odre le cadre de lecture 0,+1,+2 puis on change de brin et on remodifie le cadre de lecture
 			if(cadre_lecture>3){
 				cadre_lecture = 0;
 				brin_sens = FALSE;
@@ -92,16 +230,11 @@ void recherche_sequence_codante(char sequence[], int taille){
 								seq_complet = FALSE; //FALSE car la séquence n'est pas complete donc on ne la prend pas en compte
 								break;
 							}
-							if((sequence[i]=='\0')){
+							if((sequence[i+2]=='\0')){
 								fin=TRUE;
 								seq_complet = FALSE;
 								break;
 							}
-				/*		while (((sequence[i]!='T') && (sequence[i+1]!='A') && (sequence[i+2]!='A')) ||
-	      	 			((sequence[i]!='T') && (sequence[i+1]!='A') && (sequence[i+2]!='G')) || 
-					((sequence[i]!='T') && (sequence[i+1]!='G') && (sequence[i+2]!='A'))||
-					((sequence[i]=='\0')||(sequence[i+1]=='\0')||(sequence[i+2]=='\0'))){ // on cherche le codon stop après avoir trouvé un codon start
-*/
 							i= i+3;
 							j= j+3;	
 						}	
@@ -111,8 +244,14 @@ void recherche_sequence_codante(char sequence[], int taille){
 						j=0;
 						seq_complet = TRUE;
 					}
-					i++;
-						
+					i=i+3;
+					if(max<j){ // si on a une cds de taille plus importante
+	                	                 max = j;
+        	                	         printf("test max %d\n",max);// test de la plus grande
+						 max_brin = brin_sens;
+                	               		 position = k;
+                         		}
+	
 				}	
 			}
 			else{
@@ -151,7 +290,7 @@ void recherche_sequence_codante(char sequence[], int taille){
 								seq_complet = FALSE; //FALSE car la séquence n'est pas complete donc on ne la prend pas en compte
 								break;
 							}
-							if((sequence_comp[i]=='\0')){
+							if((sequence_comp[i+2]=='\0')){
 								fin=TRUE;
 								seq_complet = FALSE;
 								break;
@@ -166,15 +305,23 @@ void recherche_sequence_codante(char sequence[], int taille){
 						seq_complet = TRUE;
 					}
 					i++;
-				}
-				fin=FALSE;
-			}
-			if(max<j){ // si on a une cds de taille plus importante
+					printf("on y est "); // test pour voir si on rentre dans la boucle 
+					if(max<j){ // si on le max est inférieur au nombre de nt trouvé dans une cds j'enregistre les données de la cds
+	                                	max = j;// ici le nombre de nt max
+						printf("test max anti-sens %d\n",max);
+	                                	max_brin = brin_sens; //le brin sur lequel il se situe
+						position = k; //la position du premier nt de la cds
+	                        	}
+
+				}*/
+		/*		fin=FALSE;
+			}*/
+			/*if(max<j){ // si on a une cds de taille plus importante
 				max = j;
 				max_brin = brin_sens;
 				position = k;
-			}
-			if ((sequence[i]=='\0')||(sequence[i+1]=='\0')||(sequence[i+2]=='\0')){ // dans le cas ou on est à la fin de la séquence
+			}*/
+		/*	if ((sequence[i]=='\0')||(sequence[i+1]=='\0')||(sequence[i+2]=='\0')){ // dans le cas ou on est à la fin de la séquence
 				cadre_lecture = cadre_lecture + 1;
 				debut=TRUE;
 			}
@@ -185,21 +332,21 @@ void recherche_sequence_codante(char sequence[], int taille){
 			i=i+1;
 			if ((brin_sens == FALSE)&&(cadre_lecture =3)){
 				fin = TRUE;
-			}
-		}
+			}*/
+	//	}
 		
 		if(max!=0){// on verifie que notre fonction fonctionne en ayant trouvé au moins une CDS
 			i=0;	//on réinitialise notre compteur
-			k=position;
+			int x = position;
 			if(max_brin == TRUE){
-				while (i<max-1){	// j étant le nombre de nucléotides tant que notre compteur est infrieur ou égal au nombre de nucléotide max on boucle
-					if(sequence[k] == '\n'){
-						seq[i] = sequence[k];
+				while (i<max){	// j étant le nombre de nucléotides tant que notre compteur est infrieur ou égal au nombre de nucléotide max on boucle
+					if(sequence[x] == '\n'){
+						seq[i] = sequence[x];
 					//seq[i]=sequence[k];
-					k++;	
+					x++;	
 					}
-					seq[i] = sequence[k];
-					k++;
+					seq[i] = sequence[x];
+					x++;
 					i++;
 
 				}
@@ -207,15 +354,15 @@ void recherche_sequence_codante(char sequence[], int taille){
 			}
 			else{
 				while (i<=max){	// j étant le nombre de nucléotides tant que notre compteur est infrieur ou égal au nombre de nucléotide max on boucle
-					seq[i] = sequence_comp[k];
+					seq[i] = sequence_comp[x];
 					i++;
-					k++;
+					x++;
 				}
 				seq[i]='\0';
 			}
 
-			printf("test : %s\n",sequence);
-			printf("\nLa CDS se situe sur le brin: %d\t (0 si brin sens et 1 si brin complémentaire)\n",max_brin);
+		//	printf("test : %s\n",sequence);
+			printf("\nLa CDS se situe sur le brin: %d\t (0 si brin sens et 1 si brin complémentaire)\nEt sur le cadre de lecture: +%d\n",max_brin,cadre_max);
 			printf("\nCDS:5'-%s-3'\n",seq);
 			char path_output[10]="CDS.fasta";
 			save_sequence(path_output,seq);
--- utils.h
+++ utils.h
@@ -7,12 +7,13 @@
 void stocker_nom_fichier(char* nom_fichier);
 void extract_sequence(FILE* fichier, char sequence[]);
 void save_sequence(const char* path_output, char* sequence);	
+int calcul_taille_fichier(FILE* fichier);
 
 
 // recherche de sequence codante
 void sequence_complementaire(char sequence[], int taille,char sequence_comp[]);
 void recherche_sequence_codante(char sequence[],int taille);
-
+int recherche_max_cds(char sequence[], int* position, int i);
 // traduction
 //void traduction(char sequence[], int taille);
 
