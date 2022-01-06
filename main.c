8cf862f1c168b6efb37cf3baf6dc5f6a1aa5007e
Maisen <maisen992gmail.com>
2022-01-06 22:16:50 +0100

    correction de conflits

--- main.c
+++ main.c
@@ -14,14 +14,23 @@ int main(){
 		printf("Le fichier:\t%s à été charger avec succès\n",nom_fichier1);
 	}
 	int taille_fichier1= calcul_taille_fichier(fichier1);
+<<<<<<< Updated upstream
 
 	char* sequence= malloc(sizeof(char)*taille_fichier1);// taille maximum qui seras réduit plus tard	
+=======
+	char* sequence= malloc(sizeof(char)*taille_fichier1);// taille maximum qui seras réduit plus tard
+>>>>>>> Stashed changes
 	extract_sequence(fichier1, sequence);
 	//printf("test bis");
 	fclose(fichier1);
 
+<<<<<<< Updated upstream
 	recherche_sequence_codante(sequence,taille_fichier1);	
   //module 2	
+=======
+	recherche_sequence_codante(sequence,taille_fichier1);
+//module 2
+>>>>>>> Stashed changes
 	char* nom_fichier2 = malloc(sizeof(char));
 	stocker_nom_fichier(nom_fichier2);
 	FILE* fichier2 = fopen(nom_fichier2,"r");
@@ -33,11 +42,18 @@ int main(){
 		printf("Le fichier:\t%s à été charger avec succès\n",nom_fichier2);
 	}
 	int taille_fichier2 = calcul_taille_fichier(fichier2);
+<<<<<<< Updated upstream
 
 	char* cds = malloc(sizeof(char)*taille_fichier2);	
 	extract_sequence(fichier2,cds);
 	transcription(cds,taille_fichier2);
   //module 3	
+=======
+	char* cds = malloc(sizeof(char)*taille_fichier2);
+	extract_sequence(fichier2,cds);
+	transcription(cds,taille_fichier2);
+//module 3
+>>>>>>> Stashed changes
 	char* nom_fichier3 = malloc(sizeof(char));
         stocker_nom_fichier(nom_fichier3);
         FILE* fichier3 = fopen(nom_fichier3,"r");
@@ -52,6 +68,15 @@ int main(){
         char* sequence_ARN = malloc(sizeof(char)*taille_fichier3);
         extract_sequence(fichier3,sequence_ARN);
         traduction(sequence_ARN,taille_fichier3);
+<<<<<<< Updated upstream
   //module 4
 
+=======
+	//module 4
+	module_4();
+	//module 5
+	module_5();
+	//module 6
+	module_6();
+>>>>>>> Stashed changes
 }
